0e161a0b42c72aa1410d8311e602f8a5a5f19565
MaximValishevskiyNZ <105181811+MaximValishevskiyNZ@users.noreply.github.com>
2022-06-27 18:04:03 +0700

    Update template.php

--- components/bitrix/news.list/testing/template.php
+++ components/bitrix/news.list/testing/template.php
@@ -1,20 +1,7 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
-/** @var array $arParams */
-/** @var array $arResult */
-/** @global CMain $APPLICATION */
-/** @global CUser $USER */
-/** @global CDatabase $DB */
-/** @var CBitrixComponentTemplate $this */
-/** @var string $templateName */
-/** @var string $templateFile */
-/** @var string $templateFolder */
-/** @var string $componentPath */
-/** @var CBitrixComponent $component */
 $this->setFrameMode(true);
 
 ?>
-
-<link href="styles.css" type="text/css" rel="stylesheet" />
 
 <div id="barba-wrapper">
    <div class="article-list">
